7481268f2fb02f2319952ba7ffd9b4df6465d3ae
thalay1817-hub <thalay1817@bbdu.ac.in>
2026-08-14 04:44:09 -0700

    Solve cs336-l11-hyperparameter-scaling-fit

--- study-plans/language-modeling-from-scratch/cs336-l11-hyperparameter-scaling-fit/cs336-l11-hyperparameter-scaling-fit.py
+++ study-plans/language-modeling-from-scratch/cs336-l11-hyperparameter-scaling-fit/cs336-l11-hyperparameter-scaling-fit.py
@@ -1,78 +1,77 @@
 import torch
 
-def hyperparameter_scaling_fit(
-    model_scales,
-    learning_rates,
-    batch_sizes,
-    measured_losses,
-    target_scale,
-):
-    num_scales = measured_losses.shape[0]
-    num_lr = measured_losses.shape[1]
-    num_bs = measured_losses.shape[2]
+def hyperparameter_scaling_fit(model_scales, learning_rates, batch_sizes, measured_losses, target_scale):
+    scales = model_scales.detach().clone()
+    lrs = learning_rates.detach().clone()
+    bss = batch_sizes.detach().clone()
+    losses = measured_losses.detach().clone()
+
+    num_scales = scales.shape[0]
+    num_lr = lrs.shape[0]
+    num_bs = bss.shape[0]
 
     optima = []
-    selected_lrs = []
-    selected_bss = []
-    scales_list = []
+    opt_lr_list = []
+    opt_bs_list = []
+    log_scale_list = []
 
     for i in range(num_scales):
-        grid = measured_losses[i]
-        flat = grid.reshape(-1)
-        idx = int(torch.argmin(flat).item())
+        grid = losses[i].reshape(-1)
+        idx = int(torch.argmin(grid).item())
         lr_idx = idx // num_bs
         bs_idx = idx % num_bs
 
-        scale_val = model_scales[i].item() if torch.is_tensor(model_scales) else model_scales[i]
-        lr_val = float(learning_rates[lr_idx].item())
-        bs_val = float(batch_sizes[bs_idx].item())
-        loss_val = float(grid[lr_idx, bs_idx].item())
+        lr_val = lrs[lr_idx].item()
+        bs_val = bss[bs_idx].item()
+        loss_val = losses[i, lr_idx, bs_idx].item()
+        scale_val = scales[i].item()
 
         optima.append({
-            "model_scale": scale_val,
-            "learning_rate": lr_val,
-            "batch_size": bs_val,
-            "loss": loss_val,
+            "model_scale": float(scale_val),
+            "learning_rate": float(lr_val),
+            "batch_size": float(bs_val),
+            "loss": float(loss_val),
         })
 
-        scales_list.append(float(scale_val))
-        selected_lrs.append(lr_val)
-        selected_bss.append(bs_val)
+        opt_lr_list.append(lr_val)
+        opt_bs_list.append(bs_val)
+        log_scale_list.append(scale_val)
 
-    scales_t = torch.tensor(scales_list, dtype=torch.float64)
-    lrs_t = torch.tensor(selected_lrs, dtype=torch.float64)
-    bss_t = torch.tensor(selected_bss, dtype=torch.float64)
+    log_scale = torch.log(torch.tensor(log_scale_list, dtype=torch.float64))
+    log_lr = torch.log(torch.tensor(opt_lr_list, dtype=torch.float64))
+    log_bs = torch.log(torch.tensor(opt_bs_list, dtype=torch.float64))
 
-    log_scales = torch.log(scales_t)
-    ones = torch.ones_like(log_scales)
-    design = torch.stack([ones, log_scales], dim=1)
+    X = torch.stack([torch.ones_like(log_scale), log_scale], dim=1)
 
-    def fit(log_y):
-        solution = torch.linalg.lstsq(design, log_y.unsqueeze(1)).solution
-        log_a = solution[0, 0]
-        b = solution[1, 0]
-        residuals = log_y - (log_a + b * log_scales)
-        a = torch.exp(log_a)
-        return float(a.item()), float(b.item()), residuals
+    sol_lr = torch.linalg.lstsq(X, log_lr.unsqueeze(1)).solution
+    log_a_lr = sol_lr[0, 0].item()
+    b_lr = sol_lr[1, 0].item()
 
-    log_lrs = torch.log(lrs_t)
-    log_bss = torch.log(bss_t)
+    sol_bs = torch.linalg.lstsq(X, log_bs.unsqueeze(1)).solution
+    log_a_bs = sol_bs[0, 0].item()
+    b_bs = sol_bs[1, 0].item()
 
-    lr_a, lr_b, lr_residuals = fit(log_lrs)
-    bs_a, bs_b, bs_residuals = fit(log_bss)
+    a_lr = float(torch.exp(torch.tensor(log_a_lr, dtype=torch.float64)).item())
+    a_bs = float(torch.exp(torch.tensor(log_a_bs, dtype=torch.float64)).item())
 
-    target_scale_val = float(target_scale)
-    target_lr = lr_a * (target_scale_val ** lr_b)
-    target_bs = bs_a * (target_scale_val ** bs_b)
+    pred_log_lr = log_a_lr + b_lr * log_scale
+    pred_log_bs = log_a_bs + b_bs * log_scale
+
+    learning_rate_residuals = (log_lr - pred_log_lr).to(torch.float64)
+    batch_size_residuals = (log_bs - pred_log_bs).to(torch.float64)
+
+    target_log_scale = torch.log(torch.tensor(float(target_scale), dtype=torch.float64)).item()
+    target_learning_rate = float(a_lr * (float(target_scale) ** b_lr))
+    target_batch_size = float(a_bs * (float(target_scale) ** b_bs))
 
     return {
         "optima": optima,
-        "learning_rate_coefficient": lr_a,
-        "learning_rate_exponent": lr_b,
-        "batch_size_coefficient": bs_a,
-        "batch_size_exponent": bs_b,
-        "learning_rate_residuals": lr_residuals,
-        "batch_size_residuals": bs_residuals,
-        "target_learning_rate": target_lr,
-        "target_batch_size": target_bs,
+        "learning_rate_coefficient": a_lr,
+        "learning_rate_exponent": float(b_lr),
+        "batch_size_coefficient": a_bs,
+        "batch_size_exponent": float(b_bs),
+        "learning_rate_residuals": learning_rate_residuals,
+        "batch_size_residuals": batch_size_residuals,
+        "target_learning_rate": target_learning_rate,
+        "target_batch_size": target_batch_size,
     }
