6f62566eca6441365f8612eeedebadf01d793033
thalay1817-hub <thalay1817@bbdu.ac.in>
2026-08-14 04:49:39 -0700

    Solve cs336-l12-conditional-perplexity

--- study-plans/language-modeling-from-scratch/cs336-l12-conditional-perplexity/cs336-l12-conditional-perplexity.py
+++ study-plans/language-modeling-from-scratch/cs336-l12-conditional-perplexity/cs336-l12-conditional-perplexity.py
@@ -1,11 +1,11 @@
 import torch
 
-def conditional_perplexity(
-    logits,
-    target_ids,
-    response_mask,
-):
-    selected_token_count = int(response_mask.sum().item())
+def conditional_perplexity(logits, target_ids, response_mask):
+    L = logits.detach().clone()
+    ids = target_ids.detach().clone()
+    mask = response_mask.detach().clone()
+
+    selected_token_count = int(torch.sum(mask).item())
 
     if selected_token_count == 0:
         return {
@@ -14,17 +14,17 @@ def conditional_perplexity(
             "selected_token_count": 0,
         }
 
-    log_probs = torch.log_softmax(logits, dim=-1)
-    target_log_probs = torch.gather(
-        log_probs, dim=-1, index=target_ids.unsqueeze(-1)
-    ).squeeze(-1)
+    log_probs = torch.log_softmax(L.to(torch.float64), dim=-1)
+    target_log_probs = torch.gather(log_probs, dim=-1, index=ids.unsqueeze(-1)).squeeze(-1)
 
-    selected_log_probs = target_log_probs[response_mask]
-    nll = -selected_log_probs.sum() / selected_token_count
-    ppl = torch.exp(nll)
+    selected = target_log_probs[mask]
+    nll = -torch.mean(selected)
+
+    negative_log_likelihood = float(nll.item())
+    perplexity = float(torch.exp(nll).item())
 
     return {
-        "negative_log_likelihood": float(nll.item()),
-        "perplexity": float(ppl.item()),
+        "negative_log_likelihood": negative_log_likelihood,
+        "perplexity": perplexity,
         "selected_token_count": selected_token_count,
     }
