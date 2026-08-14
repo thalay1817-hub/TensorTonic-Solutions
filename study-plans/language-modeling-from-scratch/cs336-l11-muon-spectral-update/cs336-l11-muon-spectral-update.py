d0d199f382def310af31b2e81455417b1474d4ce
thalay1817-hub <thalay1817@bbdu.ac.in>
2026-08-14 04:45:48 -0700

    Solve cs336-l11-muon-spectral-update

--- study-plans/language-modeling-from-scratch/cs336-l11-muon-spectral-update/cs336-l11-muon-spectral-update.py
+++ study-plans/language-modeling-from-scratch/cs336-l11-muon-spectral-update/cs336-l11-muon-spectral-update.py
@@ -1,27 +1,29 @@
 import torch
 
-def muon_spectral_update(
-    parameter,
-    gradient,
-    previous_momentum,
-    momentum_coefficient,
-    learning_rate,
-):
+def muon_spectral_update(parameter, gradient, previous_momentum, momentum_coefficient, learning_rate):
     orig_dtype = parameter.dtype
     device = parameter.device
 
-    new_momentum = momentum_coefficient * previous_momentum + gradient
+    W = parameter.detach().clone()
+    G = gradient.detach().clone()
+    B_prev = previous_momentum.detach().clone()
 
-    momentum_f32 = new_momentum.to(torch.float32)
-    u, _, vh = torch.linalg.svd(momentum_f32, full_matrices=False)
-    orthogonalized_f32 = u @ vh
+    # Compute new momentum
+    new_momentum = momentum_coefficient * B_prev + G
 
-    orthogonalized_update = orthogonalized_f32.to(orig_dtype)
+    # Compute SVD in float32
+    B_f32 = new_momentum.to(torch.float32)
+    u, _, vh = torch.linalg.svd(B_f32, full_matrices=False)
 
-    new_parameter = parameter - learning_rate * orthogonalized_update
+    O = u @ vh  # orthogonalized update in float32
+
+    orthogonalized_update = O.to(orig_dtype).to(device)
+
+    new_parameter = (W - learning_rate * O.to(orig_dtype)).to(orig_dtype).to(device)
+    new_momentum = new_momentum.to(orig_dtype).to(device)
 
     return {
-        "new_parameter": new_parameter.to(orig_dtype).to(device),
-        "new_momentum": new_momentum.to(orig_dtype).to(device),
-        "orthogonalized_update": orthogonalized_update.to(orig_dtype).to(device),
+        "new_parameter": new_parameter,
+        "new_momentum": new_momentum,
+        "orthogonalized_update": orthogonalized_update,
     }
